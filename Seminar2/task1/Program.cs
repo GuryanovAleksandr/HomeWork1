b9de77466448a15bb3a86197d8ee432400ec1552
Guryanov <sanyaguru@gmail.com>
2024-02-09 00:00:33 +0800

    add task1

---
+++ Seminar2/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 7 ==0 && number %23 ==0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23 одновременно");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно 7 и 23 одновременно");
+}
