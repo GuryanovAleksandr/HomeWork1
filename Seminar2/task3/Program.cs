bd7fff37934b1d51199b3e3b6c64379801c8f926
Guryanov <sanyaguru@gmail.com>
2024-02-09 15:40:46 +0800

    add task3

---
+++ Seminar2/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.Write("Введите двузначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if(firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
