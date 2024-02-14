b7d869be699a8ffaa09b77013952eff00f81592c
Guryanov <sanyaguru@gmail.com>
2024-02-14 11:09:47 +0800

    add seminar4: task3

---
+++ seminar4/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int[] arr = { 1, 2, 3, 4, 5 }; // Исходный массив
+int position;
+Console.WriteLine($"Массив до: [ {string.Join("; ", arr)} ]");
+for (int i = 0; i < arr.Length / 2; i++)
+{
+    position = arr[i];
+    arr[i] = arr[arr.Length - 1 - i];
+    arr[arr.Length - 1 - i] = position;
+}
+Console.WriteLine($"Массив после: [ {string.Join("; ", arr)} ]");
